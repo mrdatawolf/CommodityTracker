d9dd85a712130c169748eab4604a7fda816bb009
Patrick Moon <pmoon@letsgolearn.com>
2019-09-27 23:08:34 -0700

    adding to simple sanatizer

--- update.php
+++ update.php
@@ -20,30 +20,34 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$whatToWorkOn   = $_POST['dataType'];
-$id             = $_POST['commodity_id'];
-$stationId      = $_POST['station_id'];
-$factionId      = $_POST['faction_id'];
-$storeId        = $_POST['store_id'];
-$amount         = $_POST['amount'];
+$whatToWorkOn   = preg_replace('/[^A-Za-z0-9 ]/', '', $_POST['dataType']);
+$id             = preg_replace('/[^A-Za-z0-9 ]/', '', $_POST['commodity_id']);
+$stationId      = preg_replace('/[^A-Za-z0-9 ]/', '', $_POST['station_id']);
+$factionId      = preg_replace('/[^A-Za-z0-9 ]/', '', $_POST['faction_id']);
+$storeId        = preg_replace('/[^A-Za-z0-9 ]/', '', $_POST['store_id']);
+$amount         = preg_replace('/[^A-Za-z0-9 ]/', '', $_POST['amount']);
 
-//we are going to upsert based on the data we receive
-$sql = "SELECT count(*) FROM Commodities_Amount WHERE commodity_id=$id AND station_id=$stationId and faction_id=$factionId AND store_id=$storeId";
-if(mysqli_query($conn, $sql) < 1) {
-    $sql = "UPDATE Commodities_Amount SET amount = $amount WHERE station_id=$stationId AND faction_id=$factionId AND store_id=$storeId";
-    if(mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+switch ($whatToWorkOn) {
+    case ('commodity') :
+        //we are going to upsert based on the data we receive
+        $sql = "SELECT count(*) FROM Commodities_Amount WHERE commodity_id=$id AND station_id=$stationId and faction_id=$factionId AND store_id=$storeId";
+        if (mysqli_query($conn, $sql) < 1) {
+            $sql = "UPDATE Commodities_Amount SET amount = $amount WHERE station_id=$stationId AND faction_id=$factionId AND store_id=$storeId";
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        } else {
+            $sql = "INSERT INTO Commodities_Amount (commodity_id, station_id, faction_id, store_id, amount) VALUES ($id, $stationId, $factionId, $amount)";
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        }
+        break;
+    default :
+        echo "Error: nothing matched";
 }
-else {
-    $sql = "INSERT INTO Commodities_Amount (commodity_id, station_id, faction_id, store_id, amount) VALUES ($id, $stationId, $factionId, $amount)";
-    if(mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-}
-
 mysqli_close($conn);
