e942bf3cb80eab59a793e14c3e09114d954799e9
Patrick Moon <pmoon@letsgolearn.com>
2019-09-29 16:22:16 -0700

    gets frontend working

--- common.php
+++ common.php
@@ -4,18 +4,30 @@
  * @return PDO|null
  */
 function dbConnect($dbType) {
-    if($dbType === 'mysql') {
-        $hostname = "localhost";
-        $username = "freder26_php";
-        $password = "don't post passwords in discord";
-        $dbname = "freder26_drup354";
-        $conn = new PDO('mysql:host='. $hostname . ';dbname='. $dbname, $username, $password);
-    } elseif($dbType === 'sqlite') {
-        $conn = new PDO('sqlite:./commodityTracker.sqlite');
-    }
-    else {
-        $conn = null;
-    }
+    try{
+        if($dbType === 'mysql') {
+            $hostname = "localhost";
+            $username = "freder26_php";
+            $password = "don't post passwords in discord";
+            $dbname = "freder26_drup354";
+            $conn = new PDO('mysql:host='. $hostname . ';dbname='. $dbname, $username, $password);
+        } elseif($dbType === 'sqlite') {
+            $conn = new PDO('sqlite:./commodityTracker.sqlite');
+        }
+        else {
+            $conn = null;
+        }
     
-    return $conn;
+        return $conn;
+    }
+    catch(PDOException $ex){
+        die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+    }
+}
+
+function dd($input) {
+    echo "<pre>";
+    print_r($input);
+    echo "</pre>";
+    die();
 }
